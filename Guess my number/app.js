48150150419b73aca374d5c050315beea858f9c9
M-Ahmadinno <m.ahmadtariq1999@gmail.com>
2026-01-14 12:36:55 +0500

    guess my number complete

--- Guess my number/app.js
+++ Guess my number/app.js
@@ -6,20 +6,29 @@ var highscore = 0
 var highscoreUI = document.querySelector("#highscore")
 var myText = document.querySelector("#myText")
 function check () {
-    var myGuess = document.querySelector("input")
-    if (ramNumber == myGuess.value ){
+    var myGuess = document.querySelector("input").value
+    if (ramNumber == myGuess ){
          myText.textContent = "You Win"
         scoreUI.textContent = 20
         highscore = score
         highscoreUI.textContent = highscore
     }else{
+        const difference = ramNumber - myGuess
+        if(difference < 0 ){
+          if(difference == -1 || difference == -2 ){
+             myText.textContent = "Your Guess is High,But very close"
+           }else{
+             myText.textContent =  "Your Guess is High"
+           }
+        }else{
+          if(difference == 1 || difference == 2 ){
+            myText.textContent =  "Your Guess is Low,But very close" 
+          }else{
+            myText.textContent = "Your Guess is Low" 
+          }
+        }
         score--
         scoreUI.textContent = score
-        if(myGuess.value > ramNumber){
-          myText.textContent = "Your Guess is High"
-        }else{
-          myText.textContent = "Your Guess is Low"  
-        }
     }
 }
 function again() {
